1627121ba0dfd803a57a597660c90f400b5d62ab
SMishra <smishra@kenmarkitan.com>
2024-05-18 12:38:43 +0530

    updated port

--- src/Helpers/axiosinstance.js
+++ src/Helpers/axiosinstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL=" http://localhost:9036/api/v1/";
+const BASE_URL="https://lmsbackend-2hw4iobqm-shashanks-projects-3027ed5d.vercel.app/api/v1";
 const axiosInstance=axios.create();
 
 axiosInstance.defaults.baseURL=BASE_URL;

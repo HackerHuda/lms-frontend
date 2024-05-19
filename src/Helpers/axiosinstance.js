e52b9182cff854a801b93afa22ea39f2618b0471
SMishra <smishra@kenmarkitan.com>
2024-05-19 23:34:49 +0530

    deploy

--- src/Helpers/axiosinstance.js
+++ src/Helpers/axiosinstance.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL="https://lmsbackend-2hw4iobqm-shashanks-projects-3027ed5d.vercel.app/api/v1";
+const BASE_URL="https://lmsbackend-26ictry8p-shashanks-projects-3027ed5d.vercel.app/api/v1";
+// const BASE_URL="http://localhost:3000/api/v1";
+
 const axiosInstance=axios.create();
 
 axiosInstance.defaults.baseURL=BASE_URL;
